01353dec00bec627d446d4640fb4e412d25a29c6
Seth Magee <sethnsd@gmail.com>
2025-03-18 23:46:10 -0400

    css improvments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ChoresChart from './ChoresChart';
 import './firebase';
+import './index.css';  // Import Tailwind CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
