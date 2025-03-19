f640aacb25395d40cf7e3aff3fc01bc6b0e0dc09
Seth Magee <sethnsd@gmail.com>
2025-03-19 00:04:00 -0400

    tailwind files 7

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import ChoresChart from './ChoresChart';
 import './firebase';
 import './index.css';  // Import Tailwind CSS
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
